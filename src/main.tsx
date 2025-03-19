b363e53c6b0ee9b06ab49813a3bc074017592bf7
Keegan Williams <KeeganW@users.noreply.github.com>
2025-03-18 22:25:36 -0700

    Basic page layout.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <HashRouter>
     <App />
-  </StrictMode>,
-)
+  </HashRouter>
+);
