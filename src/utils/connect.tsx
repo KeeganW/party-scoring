e7c0c0950e4d03eab045473fdd781021b1dd364c
Keegan Williams <KeeganW@users.noreply.github.com>
2025-03-25 23:01:17 -0700

    Changing order.

--- src/utils/connect.tsx
+++ src/utils/connect.tsx
@@ -3,12 +3,12 @@ import {PlayerType} from './types';
 
 export const players: PlayerType[] = [
   { name: 'Jedd', id: 310, color: 'cyan' },
-  { name: 'Nick', id: 311, color: 'red' },
-  { name: 'Matt', id: 190, color: 'green' },
-  { name: 'Ken', id: 312, color: 'yellow' },
-  { name: 'Max', id: 313, color: 'purple' },
-  { name: 'Mike', id: 314, color: 'gray' },
   { name: 'Keegan', id: 1, color: 'orange' },
+  { name: 'Ken', id: 312, color: 'purple' },
+  { name: 'Matt', id: 190, color: 'green' },
+  { name: 'Max', id: 313, color: 'yellow' },
+  { name: 'Mike', id: 314, color: 'gray' },
+  { name: 'Nick', id: 311, color: 'red' },
 ];
 
 export interface CheersGovernorScore {
