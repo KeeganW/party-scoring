126eedd0c968915e51496849c501fa649fe326ce
Keegan Williams <KeeganW@users.noreply.github.com>
2025-04-07 21:17:49 -0700

    Adding colors to games.

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -3,15 +3,15 @@ import { Link, useNavigate } from 'react-router';
 import { IconArrowLeft, IconBeer, IconCrown, IconIceSkating, IconGlassFullFilled, IconWand, IconPig, IconBus, IconDice5, IconRipple } from '@tabler/icons-react';
 
 const buttons = [
-  { text: 'Beer Die', link: '/beer_die', icon: <IconBeer /> },
-  { text: 'Cheers Governor', link: '/cheers_governor', icon: <IconGlassFullFilled /> },
-  { text: 'Hockey', link: '/hockey', icon: <IconIceSkating /> },
-  { text: 'Kings Cup', link: '/kings_cup', icon: <IconCrown /> },
-  { text: 'Magical Mixers', link: '/magical_mixers', icon: <IconWand /> },
-  { text: 'Pass the Pigs', link: '/pass_the_pigs', icon: <IconPig /> },
-  { text: 'Ride the Bus', link: '/ride_the_bus', icon: <IconBus /> },
-  { text: 'Snappa', link: '/snappa', icon: <IconDice5 /> },
-  { text: 'Wavelength', link: '/wavelength', icon: <IconRipple /> },
+  { text: 'Beer Die', link: '/beer_die', icon: <IconBeer />, color: 'green' },
+  { text: 'Cheers Governor', link: '/cheers_governor', icon: <IconGlassFullFilled />, color: 'orange' },
+  { text: 'Hockey', link: '/hockey', icon: <IconIceSkating />, color: 'blue' },
+  { text: 'Kings Cup', link: '/kings_cup', icon: <IconCrown />, color: 'yellow' },
+  { text: 'Magical Mixers', link: '/magical_mixers', icon: <IconWand />, color: 'black' },
+  { text: 'Pass the Pigs', link: '/pass_the_pigs', icon: <IconPig />, color: 'cyan' },
+  { text: 'Ride the Bus', link: '/ride_the_bus', icon: <IconBus />, color: 'red' },
+  { text: 'Snappa', link: '/snappa', icon: <IconDice5 />, color: 'lime' },
+  { text: 'Wavelength', link: '/wavelength', icon: <IconRipple />, color: 'violet' },
 ];
 
 export const Games = () => {
@@ -32,7 +32,7 @@ export const Games = () => {
             <Stack>
               {buttons.map((button, index) => (
                 <Group key={index} grow>
-                  <Button component={Link} to={button.link}>
+                  <Button component={Link} to={button.link} color={button.color} >
                     <div style={{ marginRight: '1rem' }}>
                       {button.icon}
                     </div>
